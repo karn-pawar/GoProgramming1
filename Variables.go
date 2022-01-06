81cbc1a83fdb5ece64d7545e755ae5965184388f
karn pawar <karnpawar2271@gmail.com>
2022-01-06 22:28:56 +0530

    init

--- Variables.go
+++ Variables.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 	//in golang we have two method to declare variables
+	//First Method
 	var a int = 10
 	var name string = "Karn"
 	var boll bool = false
 	fmt.Println(a, name, boll)
+	//Second method
+	m := 88
+	n := 99
+	sirname := "Pawar"
+	fmt.Println(m, n, sirname)
 
 }
